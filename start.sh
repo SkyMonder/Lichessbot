4f93f7609ad607e8ddf851f0646a59bc0d79baa2
SkyMonder <rilloperdagelo@gmail.com>
2026-03-21 22:35:12 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,15 +5,10 @@ echo "Скачиваем Stockfish 17.1..."
 wget -q https://github.com/official-stockfish/Stockfish/releases/download/sf_17.1/stockfish-ubuntu-x86-64.tar
 tar -xf stockfish-ubuntu-x86-64.tar
 
-# Перемещаем бинарник в корень и даём права
-if [ -f stockfish/stockfish-ubuntu-x86-64 ]; then
-    mv stockfish/stockfish-ubuntu-x86-64 ./stockfish
-    chmod +x ./stockfish
-    rm -rf stockfish   # удаляем ненужную папку
-else
-    echo "Не найден бинарник Stockfish"
-    exit 1
-fi
+# Перемещаем бинарник из папки в корень и удаляем папку
+mv stockfish/stockfish-ubuntu-x86-64 ./stockfish
+rm -rf stockfish
+chmod +x ./stockfish
 
 echo "Запускаем бота..."
 exec gunicorn -k uvicorn.workers.UvicornWorker -b 0.0.0.0:$PORT bot:app
