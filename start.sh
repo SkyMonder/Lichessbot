74b51f4f1f7f1b1f52cebbcc62abcab73cc4166e
SkyMonder <rilloperdagelo@gmail.com>
2026-03-21 22:13:16 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-# Скачиваем исходники и компилируем Stockfish
-wget https://github.com/official-stockfish/Stockfish/archive/sf_17.1.tar.gz
-tar -xzf sf_17.1.tar.gz
-cd Stockfish-sf_17.1/src
-make -j2 build ARCH=x86-64
-cp stockfish ../../
-cd ../..
-chmod +x stockfish
+# Скачиваем готовый бинарник Stockfish (Linux, x86-64, modern)
+wget https://github.com/official-stockfish/Stockfish/releases/download/sf_17.1/stockfish-ubuntu-x86-64.tar
+tar -xf stockfish-ubuntu-x86-64.tar
+chmod +x stockfish/stockfish-ubuntu-x86-64
+# Копируем в корень для удобства
+cp stockfish/stockfish-ubuntu-x86-64 stockfish
 
 # Запускаем бота
 gunicorn -k uvicorn.workers.UvicornWorker -b 0.0.0.0:$PORT bot:app
