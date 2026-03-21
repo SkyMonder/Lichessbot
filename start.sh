a40274e5c4b571dd6393ead117645d6a77014331
SkyMonder <rilloperdagelo@gmail.com>
2026-03-21 22:02:44 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-# Устанавливаем Stockfish на Render
-apt-get update && apt-get install -y stockfish
-# Запускаем бота с HTTP-сервером для health check
+# Скачиваем исходники и компилируем Stockfish
+wget https://github.com/official-stockfish/Stockfish/archive/sf_17.1.tar.gz
+tar -xzf sf_17.1.tar.gz
+cd Stockfish-sf_17.1/src
+make -j2 build ARCH=x86-64
+cp stockfish ../../
+cd ../..
+chmod +x stockfish
+
+# Запускаем бота
 gunicorn -k uvicorn.workers.UvicornWorker -b 0.0.0.0:$PORT bot:app
