0a052342bd4c9ad0d502d580d05d9015ad9bf71c
SkyMonder <rilloperdagelo@gmail.com>
2026-04-19 21:47:21 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
 set -e
 
-echo "Скачиваем Stockfish 17.1..."
-wget -q https://github.com/official-stockfish/Stockfish/releases/download/sf_17.1/stockfish-ubuntu-x86-64.tar
-tar -xf stockfish-ubuntu-x86-64.tar --strip-components=1 -C . stockfish/stockfish-ubuntu-x86-64
-mv stockfish-ubuntu-x86-64 stockfish
-chmod +x stockfish
+echo "Скачиваем и настраиваем движки..."
 
-echo "Пытаемся превратить аккаунт в бота..."
+# --- Stockfish 18 (основной) ---
+wget -q https://github.com/official-stockfish/Stockfish/releases/download/sf_18/stockfish-ubuntu-x86-64-bmi2.tar
+tar -xf stockfish-ubuntu-x86-64-bmi2.tar
+cp stockfish/stockfish-ubuntu-x86-64-bmi2 ./stockfish
+rm -rf stockfish
+chmod +x ./stockfish
+
+# --- Berserk (атакующий) ---
+wget -q https://github.com/jhonnold/berserk/releases/download/20250218/berserk-20250218-linux.zip
+unzip -j berserk-20250218-linux.zip "berserk-20250218-linux/berserk" -d .
+mv berserk berserk_engine
+chmod +x ./berserk_engine
+
+# --- Clover (сбалансированный) ---
+wget -q https://github.com/lucasmartin/Clover/releases/download/3.0.3/clover_3.0.3_linux.zip
+unzip -j clover_3.0.3_linux.zip "clover_3.0.3_linux/clover" -d .
+mv clover clover_engine
+chmod +x ./clover_engine
+
+# --- Установка unzip, если отсутствует ---
+apt-get update && apt-get install -y unzip
+
+echo "Превращаем аккаунт в бота (если нужно)..."
 curl -X POST -d '' https://lichess.org/api/bot/account/upgrade \
      -H "Authorization: Bearer $LICHESS_TOKEN" \
      -H "Content-Type: application/x-www-form-urlencoded" \
