bffdae633dea720cee4e71efe18c605c7548a9ce
SkyMonder <rilloperdagelo@gmail.com>
2026-04-19 22:04:34 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,15 +3,19 @@ set -e
 
 echo "=== Установка движков ==="
 
+# --- Установка unzip (требуется для Berserk и Clover) ---
+apt-get update && apt-get install -y unzip
+
 # --- Stockfish 18 ---
 echo "Скачиваем Stockfish 18..."
 wget -q https://github.com/official-stockfish/Stockfish/releases/download/sf_18/stockfish-ubuntu-x86-64-bmi2.tar
 tar -xf stockfish-ubuntu-x86-64-bmi2.tar
+# Внутри появилась папка stockfish/ с файлом stockfish-ubuntu-x86-64-bmi2
 cp stockfish/stockfish-ubuntu-x86-64-bmi2 ./stockfish
 rm -rf stockfish
 chmod +x ./stockfish
 
-# --- Berserk (опционально, можно отключить) ---
+# --- Berserk (опционально) ---
 echo "Скачиваем Berserk..."
 wget -q https://github.com/jhonnold/berserk/releases/download/20250218/berserk-20250218-linux.zip
 unzip -j berserk-20250218-linux.zip "berserk-20250218-linux/berserk" -d .
@@ -24,9 +28,6 @@ wget -q https://github.com/lucasmartin/Clover/releases/download/3.0.3/clover_3.0
 unzip -j clover_3.0.3_linux.zip "clover_3.0.3_linux/clover" -d .
 mv clover clover_engine
 chmod +x ./clover_engine
-
-# Установка unzip, если не установлен
-apt-get update && apt-get install -y unzip
 
 echo "=== Превращаем аккаунт в бота ==="
 curl -X POST -d '' https://lichess.org/api/bot/account/upgrade \
